48627730c9d759a03cd9c5d01c4be4f1ad7e2ad8
Eric Klose <ericklose@gmail.com>
2016-03-01 15:07:16 -0500

    most setup done (github, alamofire, MVC folders)

---
+++ weather-app/File.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  weather-app
+//
+//  Created by Eric Klose on 3/1/16.
+//  Copyright © 2016 Eric Klose. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "api.openweathermap.org/data/2.5/weather?appid=3c257522fc193a6750f673e02e05bd16"
+let URL_QUERY_TYPE = "&zip="
+let URL_LOCATION = "02111,us"
+
+let URL = "\(URL_BASE)\(URL_QUERY_TYPE)\(URL_LOCATION)"
+
+typealias DownloadComplete = () -> ()
+
+
